fa257cb3969ef67e7b175b4ed4cdd8cd2fe60e4b
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-03-19 00:55:37 +0530

    Modified User Management window adding a form for user registration

--- app/UserManagement/page.jsx
+++ app/UserManagement/page.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function UserManagement() {
+  //Define state variables.
+  const [clickRegister, setClickRegister] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  //Define function for Register New Button
+  const handleRegisterNewWindow = () => {
+    if (!clickRegister) {
+      setClickRegister(true);
+    } else {
+      setClickRegister(false);
+    }
+  };
+  const handleCancel = () => {
+    if (!clickRegister) {
+      setClickRegister(true);
+    } else {
+      setClickRegister(false);
+    }
+  };
+  const handleRegister = ()=>{
+    setModal(true);
+  }
+
   return (
     <div>
       <div className="bg-slate-800 w-full h-[45px] flex flex-row items-center shadow-lg">
@@ -37,7 +61,9 @@ export default function UserManagement() {
               clipRule="evenodd"
             />
           </svg>
-          <div className="text-sm text-white hover:bg-slate-700 w-[110px] h-[32] flex flex-col items-center justify-center rounded-md">
+          <div
+            onClick={() => handleRegisterNewWindow()}
+            className="text-sm text-white hover:bg-slate-700 w-[110px] h-[32] flex flex-col items-center justify-center rounded-md">
             <button>Register New</button>
           </div>
         </div>
@@ -110,6 +136,235 @@ export default function UserManagement() {
           </div>
         </div>
       </div>
+
+      {/*Sub windows*/}
+      {clickRegister && (
+        <div className="w-[full] h-[370px] mt-4 shadow-md">
+          <div className="bg-red-800 w-full h-[30px] flex flex-row items-center">
+            <label className="text-white ml-2">Provide User details</label>
+          </div>
+          <form className="flex flex-col">
+            <div className="flex flex-row mt-4">
+              <select
+                id="small"
+                className="block w-[150px] ml-8 p-2 outline-blue-300 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option>Select Status</option>
+                <option value="Mr.">Mr.</option>
+                <option value="Mrs.">Mrs.</option>
+                <option value="Miss.">Miss.</option>
+                <option value="Prof.">Prof.</option>
+                <option value="Doc.">Doc.</option>
+              </select>
+
+              <select
+                id="small"
+                className="block w-[150px] ml-8 p-2 outline-blue-300 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option>Select User Level</option>
+                <option value="0">Administrator</option>
+                <option value="1.">Authorizer</option>
+                <option value="2">Initiator</option>
+              </select>
+            </div>
+
+            <div className="flex flex-row mt-1">
+              <div>
+                <label
+                  htmlFor="small-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white ml-8">
+                  First Name:
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter First Name"
+                  id="small-input"
+                  className="block ml-8 w-[450px] p-2 outline-blue-300 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="small-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white ml-8">
+                  Last Name:
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Last Name"
+                  id="small-input"
+                  className="block ml-8 w-[450px] p-2 outline-blue-300 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row mt-1">
+              <div>
+                <label
+                  htmlFor="small-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white ml-8">
+                  Email Address:
+                </label>
+                <input
+                  type="email"
+                  placeholder="example@slicgeneral.com"
+                  id="small-input"
+                  className="block ml-8 w-[450px] outline-blue-300 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="small-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white ml-8">
+                  EPF:
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Last Name"
+                  id="small-input"
+                  className="block ml-8 w-[450px] outline-blue-300 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row mt-1">
+              <div>
+                <label
+                  htmlFor="small-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white ml-8">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  id="small-input"
+                  className="block ml-8 w-[450px] outline-blue-300 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="small-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white ml-8">
+                  Confirm Password:
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password again"
+                  id="small-input"
+                  className="block ml-8 w-[450px] outline-blue-300 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row mt-4 ml-8">
+              <button
+              onClick={()=>{handleRegister()}}
+                type="button"
+                className="flex flex-row items-center border-none justify-center h-[30px] w-[90px] shadow-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-md text-sm text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg
+                  className="w-6 h-6 text-white dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 28 28">
+                  <path
+                    fillRule="evenodd"
+                    d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Register
+              </button>
+
+              <button
+                onClick={() => handleCancel()}
+                type="button"
+                className="flex flex-row items-center justify-center me-2 mb-2 h-[30px] w-[90px] shadow-md text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 f dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                <svg
+                  className="w-6 h-6 text-black dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 28 28">
+                  <path
+                    fillRule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+
+      {modal && (
+        <div
+          id="popup-modal"
+          tabIndex="-1"
+          className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="relative p-4 w-full max-w-md">
+            <div className="relative bg-white border rounded-lg shadow dark:bg-gray-700">
+              <button
+                type="button"
+                onClick={() => setModal(false)} // Close modal on click
+                className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14">
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+              <div className="p-4 md:p-5 text-center">
+                <svg
+                  className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20">
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                  Are you sure you want to register this user?
+                </h3>
+                <button
+                  type="button"
+                  onClick={() => setModal(false)} // Close modal on click
+                  className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                  Yes, I'm sure
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setModal(false)} // Close modal on click
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                  No, cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
