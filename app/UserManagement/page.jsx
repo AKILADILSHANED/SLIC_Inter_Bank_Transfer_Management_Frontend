8ead12395613a4a784215b95318cb7c22ad214ba
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-03-19 01:07:52 +0530

    Added Success and Error message components into Manage User Window

--- app/UserManagement/page.jsx
+++ app/UserManagement/page.jsx
@@ -21,9 +21,9 @@ export default function UserManagement() {
       setClickRegister(false);
     }
   };
-  const handleRegister = ()=>{
+  const handleRegister = () => {
     setModal(true);
-  }
+  };
 
   return (
     <div>
@@ -140,7 +140,7 @@ export default function UserManagement() {
       {/*Sub windows*/}
       {clickRegister && (
         <div className="w-[full] h-[370px] mt-4 shadow-md">
-          <div className="bg-red-800 w-full h-[30px] flex flex-row items-center">
+          <div className="bg-green-600 w-full h-[30px] flex flex-row items-center">
             <label className="text-white ml-2">Provide User details</label>
           </div>
           <form className="flex flex-col">
@@ -258,7 +258,9 @@ export default function UserManagement() {
 
             <div className="flex flex-row mt-4 ml-8">
               <button
-              onClick={()=>{handleRegister()}}
+                onClick={() => {
+                  handleRegister();
+                }}
                 type="button"
                 className="flex flex-row items-center border-none justify-center h-[30px] w-[90px] shadow-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-md text-sm text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <svg
@@ -303,6 +305,80 @@ export default function UserManagement() {
         </div>
       )}
 
+      {/*Error Message Components*/}
+      <div
+        id="alert-border-3"
+        className="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800"
+        role="alert">
+        <svg
+          className="shrink-0 w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <div className="ms-3 text-sm font-medium">Message Here....</div>
+        <button
+          type="button"
+          className="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
+          data-dismiss-target="#alert-border-3"
+          aria-label="Close">
+          <span className="sr-only">Dismiss</span>
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14">
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div
+        id="alert-border-2"
+        class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800"
+        role="alert">
+        <svg
+          class="shrink-0 w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <div class="ms-3 text-sm font-medium">Message Here....</div>
+        <button
+          type="button"
+          class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+          data-dismiss-target="#alert-border-2"
+          aria-label="Close">
+          <span class="sr-only">Dismiss</span>
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {/*Modal Component*/}
       {modal && (
         <div
           id="popup-modal"
