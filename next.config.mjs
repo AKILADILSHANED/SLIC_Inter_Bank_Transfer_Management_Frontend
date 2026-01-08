055fa54adc1ed7e75f913598357196ecb6819236
Akila Edirisooriya <edirisooriyaakila@gmail.com>
2026-01-08 17:03:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+  output: 'standalone', // or remove 'output' entirely
+  // Or force SSR for all pages:
+  // experimental: {
+  //   serverActions: {
+  //     bodySizeLimit: '2mb',
+  //   },
+  // },
+};
 
 export default nextConfig;
